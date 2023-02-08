3d1cc8edf1eb4ec94cf99b9a598f4da7810b0da4
Ryan Gibb <ryan@freumh.org>
2023-02-08 13:24:58 +0000

    nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          ocaml-lsp-server = "*";
+          ocamlformat = "*";
+          alcotest = "*";
+        };
+        scope = on.buildOpamProject' { } ./. devPackagesQuery;
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
+      in {
+        legacyPackages = scope;
+
+        packages = scope;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            scope.dns-client
+            scope.dns-cli
+            scope.dns-mirage
+            scope.dns
+            scope.dns-resolver
+            scope.dnssec
+            scope.dns-server
+            scope.dns-stub
+            scope.dns-tsig
+          ];
+          buildInputs = devPackages;
+        };
+      });
+}
